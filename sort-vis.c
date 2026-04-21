4390bba69abef6b18d867cfdbf44de3e3f209119
cyb3r@debian <cyb3rrr1@gmail.com>
2026-04-21 15:40:34 -0400

    added bubble sort made it pretty

--- sort-vis.c
+++ sort-vis.c
@@ -1,40 +1,65 @@
 // https://www.youtube.com/watch?v=bm-UnFrLajU&t=1277s
 
+#include <stdlib.h>
 #include <raylib.h>
 
-#define WIDTH 900
-#define HEIGHT 600
-#define NUM_BAR 50
+#define WIDTH   900
+#define HEIGHT  600
+#define NUM_BARS 50
 
-void bars[NUM_BARS];
+// prototypes
+void init_bars();
+void draw_bars(int highlight);
+void bubble_sort_step();
 
-int main ()
-  {
-    InitWindowShouldClose())
-    {
-      while (!WindowShouldCose())
-      bubble_sort_step();
+int bars[NUM_BARS];
+int sorted = 0;
+int pass   = 0;
 
-      BeginDrawing();
+int main() {
+    InitWindow(WIDTH, HEIGHT, "Sort");
+    SetTargetFPS(10);
+    init_bars();
+
+    while (!WindowShouldClose()) {
+        bubble_sort_step();
+
+        BeginDrawing();
+            ClearBackground(BLACK);
+            draw_bars(pass);
+        EndDrawing();
     }
-  }
 
-
-
-
-// gobal array
-void intit_bars()
-{
-  for (int i = 0; 1 < NUM_BARS; i++)
-    bars[i] = GetRandomValue(10, HEIGHT - 10);
+    CloseWindow();
+    return 0;
 }
 
-void draw_bars(int highlights )
-{
-  int bar_width = WIDTH / NUM_BARS;
-  for (int i = 0; i < NUM_BARS; i++)
-    {
-      Color color = (i = highlight) ? RED : RAYWHITE;
-      DrawRectangle(i * bar_width, HEIGHT - bars[i], bar_width - 2, ) bars[i], color);
+//TODO:
+//needs tbe set in a seperate file.... or keep it here idk
+void bubble_sort_step() {
+    if (sorted) return;
+    int swapped = 0;
+    for (int i = 0; i < NUM_BARS - 1 - pass; i++) {
+        if (bars[i] > bars[i + 1]) {
+            int tmp  = bars[i];
+            bars[i]  = bars[i + 1];
+            bars[i + 1] = tmp;
+            swapped  = 1;
+        }
+    }
+    pass++;
+    if (!swapped) sorted = 1;
+}
+
+void init_bars() {
+    for (int i = 0; i < NUM_BARS; i++)
+        bars[i] = GetRandomValue(10, HEIGHT - 10);
+}
+
+void draw_bars(int highlight) {
+    int bar_width = WIDTH / NUM_BARS;
+    for (int i = 0; i < NUM_BARS; i++) {
+        Color color = (i == highlight) ? ORANGE : RAYWHITE;
+        DrawRectangle(i * bar_width, HEIGHT - bars[i], bar_width - 2, bars[i], color);
     }
 }
